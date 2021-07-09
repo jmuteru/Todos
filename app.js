9e8c3494ca2d133f3df20d11674cefa4728e4046
Jeff Muteru <jeff.muteru@tracom.co.ke>
2021-07-09 15:15:15 +0300

    added an animated mask icon for fun.

--- app.js
+++ app.js
@@ -1,55 +1,51 @@
-const addForm = document.querySelector('.add'); //submit form
-const list = document.querySelector('.todos '); //the ul
+const list = document.querySelector('.todos');//UL
+const addTodo = document.querySelector('.add');
 const search = document.querySelector('.search input');
-// function below adds a new todo to the list
-const generateTemplate = (todo)=>{
-    
-   const html = `
-        <li class="list-group-item d-flex justify-content-between align-items-center">
-            <span>${todo}</span>
-            <i class="far fa-trash-alt delete"></i>
-        </li>
-   
-   `;
-//    adding the list item to the ul
-   list.innerHTML += html;
+const todosList = document.querySelector('.todos');
+// Adding todos function
+
+const  generateTodo = (todo)=>{
+    const todoHtml = `
+    <li class="list-group-item d-flex justify-content-between align-items-center">
+        <span>${todo}</span>
+        <i class="far fa-trash-alt  delete"></i>
+    </li>
+    `;
+    list.innerHTML += todoHtml;
 };
-addForm.addEventListener('submit', e=>{
+
+
+//Submitting the todo
+addTodo.addEventListener('submit',e=>{
     e.preventDefault();
-    const todo = addForm.add.value.trim();
-    if (todo.length) {
-        generateTemplate(todo);
-        addForm.reset();
+    const addTodoItm = addTodo.add.value.trim();
+    if (addTodoItm.length) {
+        generateTodo(addTodoItm);
 
-    } 
-}); 
-
-//deletion
-// removing item from the todo list
-//add a click eventv to trash icon that deletes a todo
-list.addEventListener('click',e=>{
-    //we want to check if the item clicked is the trash can icon
-    if (e.target.classList.contains('delete')) {
-        e.target.parentElement.remove();
-        
     }
-
+    addTodo.reset();
 });
 
-//Searching for a todo
-//We now
-const filterTodos = (searchTerm)=>{
-   Array.from(list.children)
-   .filter((todo)=> !todo.textContent.toLowerCase.includes(searchTerm))
-   .forEach((todo)=> todo.classList.add('filtered'));
+//deleting the todos
+ list.addEventListener('click', e =>{
+       if (e.target.classList.contains('delete')) {
+           e.target.parentElement.remove();
+       }
+ });
 
-   Array.from(list.children)
-   .filter((todo)=> todo.textContent.toLowerCase.includes(searchTerm))
-   .forEach((todo)=> todo.classList.remove('filtered'));
+//  Write a func that searches for the todos
+const searchTodos = (todos)=>{
+    Array.from(list.children)
+    .filter((todo)=> !todo.textContent.toLowerCase.includes(todos))
+    .forEach((todo)=>todo.classList.add('filtered'));
+
+    Array.from(list.children)
+    .filter((todo)=> todo.textContent.toLowerCase.includes(todos))
+    .forEach((todo)=>todo.classList.add('filtered'));
 };
-//Key up event
-search.addEventListener('keyup', ()=>{
-    //Getting whatever is in the input  field in at that moment and time...
-    const term = search.value.trim.toLowerCase();
-    filterTodos(term);
-});
+
+//  Searching for todos using key up event
+search.addEventListener('keyup',()=>{
+    const todoTerm = search.value.trim().toLowerCase();
+    searchTodos(todoTerm);
+    });
